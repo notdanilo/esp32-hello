0a16bda3cd85f30e90112981fd0c9b3ef7a1aa19
Danilo Guanabara <danguafer@gmail.com>
2020-06-21 12:25:00 -0300

    Cleaner Wifi impl

--- app/src/main.rs
+++ app/src/main.rs
@@ -7,22 +7,46 @@ use std::thread::sleep;
 use std::time::Duration;
 
 use esp32_hal::EspError;
+use crate::wifi::WifiClient;
+use futures::executor::block_on;
 
 mod blink;
 mod wifi;
 
 #[no_mangle]
 pub fn app_main() {
-  if let Err(err) = rust_blink_and_write() {
+  if let Err(err) = main() {
     println!("{}", err);
   }
 }
 
-fn rust_blink_and_write() -> Result<!, EspError> {
-  //blink::start_blinker();
-  wifi::start_wifi()?;
+fn main() -> Result<!, EspError> {
+  block_on(async_main())?;
+  loop {}
+}
+
+async fn async_main() -> Result<!, EspError> {
+  let ssid     = "AP Amarelo".to_string();
+  let password = "demogroni".to_string();
+  let wifi = WifiClient::new(&ssid, &password).await.expect("Couldn't connect");
+  println!("Connected to {} with {}", ssid, wifi.address());
+
+  sleep(Duration::from_secs(5));
+
+  let socket = std::net::UdpSocket::bind("0.0.0.0:34254").expect("Couldn't bind socket");
+  socket.set_broadcast(true).expect("Couldn't enable broadcast receiving");
 
   loop {
-    sleep(Duration::from_secs(5))
+    match socket.send_to("Hello".as_bytes(), "192.168.15.60:34254") {
+      Ok(_)  => {
+        let mut message : [u8;8] = [0; 8];
+        match socket.recv_from(&mut message) {
+          Ok((size,address)) => println!("Received {} bytes from {}", size, address),
+          Err(e)             => println!("Error: {}", e)
+        }
+      },
+      Err(e) => println!("{:#?}", e)
+    }
+    sleep(Duration::from_secs(1));
   }
 }
--- app/src/wifi.rs
+++ app/src/wifi.rs
@@ -14,94 +14,29 @@ mod wifi_manager;
 use wifi_manager::*;
 mod dns;
 
-pub fn start_wifi() -> Result<(), EspError> {
-    let mut nvs = NonVolatileStorage::default()?;
+pub struct WifiClient {
+    address : Ipv4Addr
+}
 
-    let wifi = Wifi::init(&mut nvs);
+impl WifiClient {
+    pub async fn new(ssid : &str, password : &str) -> Result<Self,WifiError> {
+        let mut nvs = NonVolatileStorage::default()?;
+        let wifi = Wifi::init(&mut nvs);
 
-    println!("AP started.");
+        let sta_config = StaConfig::builder()
+            .ssid(&ssid)
+            .password(&password)
+            .build();
 
-    let namespace = nvs.open("wifi")?;
-    println!("namespace: {:?}", namespace);
+        let station = wifi.into_sta(&sta_config);
 
-    thread::Builder::new()
-        .name("dns_thread".into())
-        .stack_size(8192)
-        .spawn(dns::server)
-        .unwrap();
+        let StaRunning(ip) = station.connect().await?;
+        let address = Ipv4Addr::from(ip);
 
-    thread::Builder::new()
-        .name("server_thread".into())
-        .stack_size(8192)
-        .spawn(move || block_on(async {
-            let mac = mac_address(MacAddressType::Ap);
-            let ap_ssid = format!("ESP {}", mac);
+        Ok(Self{address})
+    }
 
-            let ap_config = ApConfig::builder()
-                .ssid(&ap_ssid)
-                .build();
-
-            let mut wifi_storage = namespace;
-
-            let ssid     = Some(String::from("YOUR SSID"));
-            let password = Some(String::from("YOUR PASSWORD"));
-
-            let mut ap_running = None;
-            let mut sta_running = None;
-
-            if let (Some(ssid), Some(password)) = (ssid, password) {
-                let sta_config = StaConfig::builder()
-                    .ssid(&ssid)
-                    .password(&password)
-                    .build();
-
-                let sta = wifi.into_sta(&sta_config);
-
-                println!("Attempting to connect.");
-
-                match sta.connect().await {
-                    Ok(sta) => {
-                        let StaRunning(ip) = sta;
-                        println!("Connected to '{}' with IP '{}'.", ssid, Ipv4Addr::from(ip));
-                        sta_running = Some(sta);
-                    },
-                    Err(err) => {
-                        println!("Failed to connect.");
-                        let ap = err.wifi().into_ap(&ap_config);
-                        ap_running = Some(ap.start());
-                    }
-                }
-            } else {
-                println!("Starting Access Point '{}' …", ap_ssid);
-                let ap = wifi.into_ap(&ap_config);
-                ap_running = Some(ap.start());
-            }
-
-            let stream = TcpListener::bind(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 80)).expect("failed starting TCP listener");
-
-            loop {
-                match stream.accept() {
-                    Ok((client, addr)) => {
-                        match handle_request(client, addr, &ap_config, &mut wifi_storage, ap_running.take(), sta_running.take()).await {
-                            Ok((ap, sta)) => {
-                                ap_running = ap;
-                                sta_running = sta;
-                            },
-                            Err(err) => {
-                                eprintln!("Failed to handle request: {:?}", err);
-                            },
-                        }
-                    },
-                    Err(e) => {
-                        if e.kind() != std::io::ErrorKind::WouldBlock {
-                            eprintln!("couldn't get client: {:?}", e);
-                        }
-                    },
-                }
-
-                thread::yield_now();
-            }
-        }))
-        .unwrap();
-    Ok(())
+    pub fn address(&self) -> &Ipv4Addr {
+        &self.address
+    }
 }
